bbc48823e7922112eed5547da93bf212ea525655
김천하 <cjsgksla@gimcheonhaui-MacBookAir.local>
2023-12-01 14:51:00 +0900

    프론트엔드 개발 완료

--- client/src/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // Adjust the path based on your project structure
-ReactDOM.render(<App />, document.getElementById('root'));
